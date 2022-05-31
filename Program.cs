53fd75fe03bb307b18ede931bbfe325140202a40
vihcarvalho <106036166+vihcarvalho@users.noreply.github.com>
2022-05-30 21:20:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Projeto_2.SRC.Entities;
+
+namespace segundoprojeto
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+            Warrior warrior = new Warrior("James", 45, "Warrior");
+            Orc orc = new Orc("Azog", 50, "Orc");
+
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(warrior.Attack(6));
+            Console.WriteLine(wizard.Attack(7));
+            Console.WriteLine(warrior.Attack(3));
+            Console.WriteLine(arus.Attack(10));
+            Console.WriteLine(orc.Attack(12));
+        }
+    }
+}
